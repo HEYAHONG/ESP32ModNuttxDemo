2f8b694317a6aaf35a5c87eae856c8bad87bd282
HEYAHONG <2229388563@qq.com>
2022-11-15 10:16:05 +0800

    添加RC资源文件

--- main/main/init.c
+++ main/main/init.c
@@ -1,10 +1,18 @@
 #include "init.h"
-
+#include "stdlib.h"
+#include "stdio.h"
+#include "RC.h"
 
 
 void init(void)
 {
     printf("Nuttx App init!\r\n");
+    {
+        char *banner = (char *)RCGetHandle("banner");
+        if (banner != NULL)
+            printf("\r\n%s\r\n", banner);
+    }
+
     //调用app初始化
     app_init();
 }
