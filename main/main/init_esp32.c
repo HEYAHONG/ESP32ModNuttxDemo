0f5a584784ea283ad4d31562af238a50a9781e37
HEYAHONG <2229388563@qq.com>
2022-11-24 17:28:52 +0800

    打印一些网络信息

--- main/main/init_esp32.c
+++ main/main/init_esp32.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <sys/mount.h>
+#include "wireless/wapi.h"
 
 void app_init(void)
 {
@@ -14,4 +15,49 @@ void app_init(void)
     mount("/dev/esp32flash", "/var/esp", "spiffs", 0, NULL);
 #endif
 #endif
+
+#ifdef CONFIG_ESP32_WIFI
+    //WIFI设置
+    {
+        //获取一个套接字用于调用驱动接口
+        int sock = wapi_make_socket();
+        const char *sta = NULL;
+        const char *ap = NULL;
+#ifdef CONFIG_ESP32_WIFI_STATION
+        //仅WIFI STA
+        sta = "wlan0";
+#endif // CONFIG_ESP32_WIFI_STATION
+#ifdef CONFIG_ESP32_WIFI_SOFTAP
+        //仅WIFI AP
+        ap = "wlan0";
+#endif // CONFIG_ESP32_WIFI_SOFTAP
+#ifdef CONFIG_ESP32_WIFI_STATION_SOFTAP_COEXISTENCE
+        //WIFI STA与WIFI AP共存
+        sta = "wlan0";
+        ap = "waln1";
+#endif // CONFIG_ESP32_WIFI_STATION_SOFTAP_COEXISTENCE
+
+
+
+        if (sta != NULL)
+        {
+            //打印ip信息
+            struct in_addr ip = {0}, netmask = {0};
+            printf("Wifi station device:%s!\r\n", sta);
+            wapi_get_ip(sock, sta, &ip);
+            printf("Wifi station ip:%d.%d.%d.%d!\r\n", ((ip.s_addr >> 0) & 0xFF), ((ip.s_addr >> 8) & 0xFF), ((ip.s_addr >> 16) & 0xFF), ((ip.s_addr >> 24) & 0xFF));
+            wapi_get_netmask(sock, sta, &netmask);
+            printf("Wifi station netmask:%d.%d.%d.%d!\r\n", ((netmask.s_addr >> 0) & 0xFF), ((netmask.s_addr >> 8) & 0xFF), ((netmask.s_addr >> 16) & 0xFF), ((netmask.s_addr >> 24) & 0xFF));
+        }
+
+        if (ap != NULL)
+        {
+            printf("Wifi softap device:%s!\r\n", ap);
+        }
+
+        //关闭套接字
+        close(sock);
+    }
+#endif
+
 }
